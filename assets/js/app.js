47fb4d25126497fd982863d341a1eff2a1d1d40d
mhoef <mhoefer1@gmail.com>
2019-01-25 22:02:49 -0500

    header work

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,17 @@
 $("#contact").hide();
-  
-$("#portfolio").hide(); 
-$(document).ready(function () {
+$("#portfolio").hide();
 
-}); 
+$(document).ready(function () {
+  $(document).on("click", "#home-button", showHome);
+  $(document).on("click", "#projects-button", showProjects);
+});
+
+function showHome() {
+  $("#portfolio").hide();
+  $("#bio").show();
+}
+
+function showProjects() {
+  $("#portfolio").show();
+  $("#bio").hide();
+}
