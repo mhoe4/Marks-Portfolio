3577781021469d2b3be2b12ee44ce6f8822f2972
Mark Hoe4 <mhoe4@mailfence.com>
2023-01-10 22:55:11 -0500

    background change

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,28 @@
 $("#contact").hide();
 $("#portfolio").hide();
-$("#portfolio2").hide();
+$("#resume").hide();
 
 $(document).ready(function () {
   $(document).on("click", "#about-button", showAbout);
   $(document).on("click", "#projects-button", showProjects);
   $(document).on("click", "#resume-button", openResume);
-
 });
 
 function showAbout() {
   $("#portfolio").hide();
   $("#bio").show();
+  $("#resume").hide();
 }
 
 function showProjects() {
   $("#portfolio").show();
   $("#bio").hide();
+  $("#resume").hide();
 }
 
 function openResume() {
-  window.open('assets/resume/resume.pdf'); 
+  //window.open('assets/resume/resume.pdf');
+  $("#bio").hide();
+  $("#portfolio").hide();
+  $("#resume").show();
 }
